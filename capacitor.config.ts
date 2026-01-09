95b88342e3aa0e8797e39002396d23c10285c115
okija-okija <chongshengtailang207@gmail.com>
2026-01-09 12:47:29 +0900

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.routinetimer.app',
+  appName: 'ルーティンタイマー',
+  webDir: 'www'
+};
+
+export default config;
