39a0ecec082b54e5015f5ec99cfb468ca84a4c90
okija-okija <chongshengtailang207@gmail.com>
2026-01-09 13:27:51 +0900

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,16 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
+  // アプリの一意なID（変更不可）
   appId: 'com.routinetimer.app',
+  // ホーム画面に表示されるアプリ名
   appName: 'ルーティンタイマー',
-  webDir: 'www'
+  // HTML/JSファイルが格納されているフォルダ名（超重要）
+  webDir: 'www',
+  server: {
+    // Androidでローカルファイルを正しく読み込むための設定
+    androidScheme: 'https'
+  }
 };
 
 export default config;
